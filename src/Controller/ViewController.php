7fed3498dd335601fb30ac23c614bd9d38efdc3b
Craig Rayner <craig@craigrayner.com>
2020-02-11 10:01:19 +1100

    Add View Controller
    Signed-off-by: Craig Rayner <craig@craigrayner.com>

---
+++ src/Controller/ViewController.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ *
+ * Kookaburra
+ * (c) 2020 Craig Rayner <craig@craigrayner.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * User: craig
+ * Date: 11/02/2020
+ * Time: 09:42
+ */
+
+namespace Kookaburra\Activities\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class ViewController
+ * @package Kookaburra\Activities\Controller
+ */
+class ViewController extends AbstractController
+{
+    /**
+     * view
+     * @Route("/view/", name="view")
+     * @IsGranted("ROLE_ROUTE")
+     */
+    public function view()
+    {
+        dd($this);
+    }
+}
